d13eadbbbf104cc9d920a219661a6d48d9a0e9de
Ankuxkashyap <ankitdochak0001@gmail.com>
2025-10-20 01:53:51 +0530

    update the url

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const url = "https://taskify-theta-teal.vercel.app";
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://taskify-theta-teal.vercel.app",
     methods: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
   },
@@ -33,7 +33,7 @@ const io = new Server(server, {
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://taskify-theta-teal.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "DELETE", "PUT"],
   })
